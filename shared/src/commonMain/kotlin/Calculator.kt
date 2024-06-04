2fd3da7a454bbad22d2fda55eab10cc2504521d4
Sushan Thapa <sushan.thapa1010@gmail.com>
2024-06-03 22:28:29 -0400

    Calculator Class added with the relevant functions

---
+++ shared/src/commonMain/kotlin/Calculator.kt
@@ -0,0 +1,17 @@
+class Calculator {
+    fun Add(left: Int, right: Int): Int {
+        return left + right
+    }
+
+    fun Subtract(left: Int, right: Int): Int {
+        return left - right
+    }
+
+    fun Multiply(left: Int, right: Int): Int {
+        return left * right
+    }
+
+    fun Divide(left: Int, right: Int): Int {
+        return left / right
+    }
+}
