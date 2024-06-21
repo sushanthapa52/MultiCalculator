26bfa80d9013bdbe4f090305be763bf4d53385df
Sushan Thapa <sushan.thapa1010@gmail.com>
2024-06-20 23:14:56 -0400

    onpress logic added in the functions

--- composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
+
+//main entry for acitivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +34,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Preview
-
-
 @Composable
 fun CalcView() {
     //inside of the CalcView function create the following state variables using rememberSaveable.
+
     var leftNumber by rememberSaveable { mutableStateOf(0) }
     var rightNumber by rememberSaveable { mutableStateOf(0) }
     var operation by rememberSaveable { mutableStateOf("") }
@@ -54,19 +61,20 @@ fun CalcView() {
     }
     // function to handle number button press
     fun numberPress(btnNum: Int) {
-        if (complete) {//Reset if calculation is complete
+        if (complete) {
             leftNumber = 0
             rightNumber = 0
             operation = ""
             complete = false
         }
+
         if (operation.isNotBlank() && !complete) {
             rightNumber = rightNumber * 10 + btnNum
         } else if (operation.isBlank() && !complete) {
             leftNumber = leftNumber * 10 + btnNum
         }
     }
-    //method implementd after the button is pressed
+    //method implemented after the button is pressed
     fun operationPress(op: String) {
         if (!complete) {
             operation = op
@@ -77,6 +85,7 @@ fun CalcView() {
         complete = true
     }
 
+    //code for interface
     Column(modifier = Modifier.background(Color.LightGray)) {
         Row {
             CalcDisplay( displayText)
@@ -100,21 +109,21 @@ fun CalcView() {
         }
     }
 }
-
+//Generates a row of number buttons for the calculator
 @Composable
-fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int) {
+fun CalcRow(onPress: (number: Int) -> Unit, startNum: Int, numButtons: Int) {
     val endNum = startNum + numButtons
     Row(modifier = Modifier.padding(0.dp)) {
         for (i in startNum until endNum) {
-            CalcNumericButton(number = i, display = display)
+            CalcNumericButton(onPress = onPress, number = i)
         }
     }
 }
-
+// it displays the character in the top display section
 @Composable
 fun CalcDisplay(display: String) {
     Text(
-        text = display.value,
+        text = display,
         modifier = Modifier
             .height(50.dp)
             .padding(5.dp)
@@ -122,31 +131,34 @@ fun CalcDisplay(display: String) {
     )
 }
 
+// handles when the numeric buttons are clicked
 @Composable
-fun CalcNumericButton(number: Int, display: MutableState<String>) {
+fun CalcNumericButton(onPress: (number: Int) -> Unit, number: Int) {
     Button(
-        onClick = { display.value += number.toString() },
+        onClick = { onPress(number) },
         modifier = Modifier.padding(4.dp)
     ) {
         Text(text = number.toString())
     }
 }
 
+
+// logic when operations button are pressed
 @Composable
-fun CalcOperationButton(operation: String, display: MutableState<String>) {
+fun CalcOperationButton(onPress: (operation: String) -> Unit, operation: String) {
     Button(
-        /* logic will be added here when button is pressed*/
-        onClick = {  },
+        onClick = { onPress(operation) },
         modifier = Modifier.padding(4.dp)
     ) {
         Text(text = operation)
     }
 }
 
+//logic when equal button is pressed
 @Composable
-fun CalcEqualsButton(display: MutableState<String>) {
+fun CalcEqualsButton(onPress: () -> Unit) {
     Button(
-        onClick = { display.value = "0" },
+        onClick = { onPress() },
         modifier = Modifier.padding(4.dp)
     ) {
         Text(text = "=")
